e7f131d6474afda9d4ac96b4a3b8bf75e9e3fdc0
Masato Sugiyama <public@smasato.net>
2022-11-08 18:43:35 +0900

    add ts & vuetify

---
+++ nuxt.config.js
@@ -0,0 +1,6 @@
+export default {
+    buildModules: [
+        '@nuxt/typescript-build',
+        '@nuxtjs/vuetify',
+    ]
+}
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-app>
+        <v-app-bar app>
+            <v-toolbar-title>Calendar Estimator</v-toolbar-title>
+        </v-app-bar>
+
+        <!-- アプリケーションのコンポーネントに基づいてコンテンツのサイズを決定 -->
+        <v-main>
+            <v-container fluid>
+            </v-container>
+        </v-main>
+
+        <v-footer app>
+        </v-footer>
+    </v-app>
+</template>
