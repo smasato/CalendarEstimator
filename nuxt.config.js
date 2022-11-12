499ff735e21a6b7cdf2114cb0e64c4e338d6eb41
Masato Sugiyama <public@smasato.net>
2022-11-12 19:37:07 +0900

    fix config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,27 @@
 export default {
+  css: [],
+  plugins: [],
+  build: {
+    postcss: {
+      syntax: "postcss-scss",
+      plugins: {
+        autoprefixer: require("autoprefixer"),
+      },
+    },
+  },
   components: true,
   buildModules: [
     "@nuxtjs/stylelint-module",
     "@nuxt/typescript-build",
     "nuxt-typed-vuex",
     "@nuxtjs/vuetify",
+    "@nuxtjs/style-resources",
+    "@nuxt/postcss8",
   ],
+  styleResources: {
+    sass: [],
+  },
+  compilerOptions: {
+    types: ["@nuxt/types"],
+  },
 };
