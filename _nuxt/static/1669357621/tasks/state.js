984d021a010e73b7f244ea927ea3d1f02d80e83a
smasato <smasato@users.noreply.github.com>
2022-11-26 01:17:36 +0000

    deploy: ee66597872c99af90f362843277f79581c5a6de6

--- _nuxt/static/1669357621/tasks/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FCalendarEstimator\u002F_nuxt\u002Fstatic\u002F1669357621",layout:"default",error:a,state:{event:{events:[]},task:{tasks:[]}},serverRendered:true,routePath:"\u002Ftasks",config:{_app:{basePath:"\u002FCalendarEstimator\u002F",assetsPath:"\u002FCalendarEstimator\u002F_nuxt\u002F",cdnURL:a}}}}(null));
