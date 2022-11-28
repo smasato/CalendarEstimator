67760379442db0aa5f45352837ebc147d6592c8c
Masato Sugiyama <public@smasato.net>
2022-11-28 17:29:20 +0900

    update pages/tasks.vue

--- pages/tasks.vue
+++ pages/tasks.vue
@@ -9,7 +9,7 @@
             <v-btn @click="resetTasks">Reset Tasks</v-btn>
           </v-col>
         </v-row>
-        <div v-for="(task, index) in tasks">
+        <div v-for="(task, index) in tasks" :key="index">
           <v-row>
             <v-col>
               <span>{{ index }}</span>
@@ -46,7 +46,12 @@ export default Vue.extend({
     };
   },
   mounted() {
-    this.tasks = this.$accessor.task.tasks;
+    this.$store.watch(
+      (state) => state.task.tasks,
+      (tasks) => {
+        this.tasks = tasks;
+      }
+    );
   },
   methods: {
     calc(task: Task) {
