6a09987895449d0cc984b12bb139e84283a6faad
Masato Sugiyama <public@smasato.net>
2022-11-29 11:51:01 +0900

    [RES-171] 実験用と開発用で表示を出し分けできるようにする (#27)

--- store/index.ts
+++ store/index.ts
@@ -3,9 +3,17 @@ import { getAccessorType } from "typed-vuex";
 import * as task from "@/store/task";
 import * as event from "@/store/event";
 
-export const state = () => ({});
-export const getters = {};
-export const mutations = {};
+export const state = () => ({
+  debug: false,
+});
+export const getters = {
+  debug: (state) => state.debug,
+};
+export const mutations = {
+  setDebug(state, debug: boolean) {
+    state.debug = debug;
+  },
+};
 export const actions = {};
 
 export const accessorType = getAccessorType({
