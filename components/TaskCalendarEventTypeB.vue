bd6840977b80a4c777d3d632b9c7711540aeae91
Masato Sugiyama <public@smasato.net>
2023-01-02 13:04:43 +0900

    update

--- components/TaskCalendarEventTypeB.vue
+++ components/TaskCalendarEventTypeB.vue
@@ -13,6 +13,9 @@ export default VCalendar.extend({
   name: "TaskCalendarEventTypeB",
   methods: {
     genTimedEvent({ event, left, width }, day) {
+      if (event.input.type === "normal") {
+        return this.genTimedEventOriginal({ event, left, width }, day);
+      }
       if (
         day.timeDelta(event.end) < 0 ||
         day.timeDelta(event.start) >= 1 ||
@@ -36,14 +39,8 @@ export default VCalendar.extend({
         timed: true,
       };
 
-      const yMaxIndex = event.input.estimateResult.histogram.y.reduce(
-        (iMax, x, i, arr) => (x > arr[iMax] ? i : iMax),
-        0
-      );
-
       const percents: { xPercent; percent }[] = [];
       event.input.estimateResult.histogram.x.forEach((x, i) => {
-        const y = event.input.estimateResult.histogram.y[i];
         const xDate = parseDate(
           dayjs(event.input.start).add(x, "minute").toDate()
         );
@@ -82,6 +79,40 @@ export default VCalendar.extend({
     // heightに対するyPositionの割合を計算する
     calcPercentPosition(yPosition: number, height: number) {
       return (yPosition / height) * 100;
+    },
+    genTimedEventOriginal({ event, left, width }, day) {
+      if (
+        day.timeDelta(event.end) < 0 ||
+        day.timeDelta(event.start) >= 1 ||
+        isEventHiddenOn(event, day)
+      ) {
+        return false;
+      }
+
+      const dayIdentifier = getDayIdentifier(day);
+      const start = event.startIdentifier >= dayIdentifier;
+      const end = event.endIdentifier > dayIdentifier;
+      const top = start ? day.timeToY(event.start) : 0;
+      const bottom = end ? day.timeToY(MINUTES_IN_DAY) : day.timeToY(event.end);
+      // @ts-ignore
+      const height = Math.max(this.eventHeight, bottom - top);
+      const scope = {
+        eventParsed: event,
+        day,
+        start,
+        end,
+        timed: true,
+      };
+      // @ts-ignore
+      return this.genEvent(event, scope, true, {
+        staticClass: "v-event-timed",
+        style: {
+          top: `${top}px`,
+          height: `${height}px`,
+          left: `${left}%`,
+          width: `${width}%`,
+        },
+      });
     },
   },
 });
