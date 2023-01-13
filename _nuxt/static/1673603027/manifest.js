941fb5c78d5512429a0fe89163b19daf19e7dfdd
smasato <smasato@users.noreply.github.com>
2023-01-13 09:43:50 +0000

    deploy: 58f282ae4679dc87e53d8aff904cc483cd01efdf

---
+++ _nuxt/static/1673603027/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fexperiment\u002Fdownload","\u002F","\u002Fdebug","\u002Fevents","\u002Ftasks","\u002Fexperiment\u002Fend","\u002Fexperiment\u002Fnormal","\u002Fexperiment\u002Fstart","\u002Fexperiment\u002Fvisualization"]})
